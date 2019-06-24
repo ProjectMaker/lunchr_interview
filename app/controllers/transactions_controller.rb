cfc70329c77975de664826ed0aeb453cab22d845
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-06-24 16:53:58 +0200

    Sort transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,27 @@
+require 'errors'
+
 class TransactionsController < ApplicationController
+  rescue_from Errors::UnpermittedParameterValue, with: :invalid_parameters
+
   def index
-    render json: Transaction.all,
+    validate_list_params
+    render json: Transaction.all.order(created_at: list_params[:sort_direction].to_s),
            status: :ok
+  end
+
+  private
+  def validate_list_params
+    sort_direction = list_params[:sort_direction]
+    has_correct_direction = ['asc', 'desc'].include? sort_direction.downcase
+    raise Errors::UnpermittedParameterValue.new(parameter: :sort_direction, value: sort_direction) unless has_correct_direction
+  end
+
+  def invalid_parameters(exception)
+    render json: { errors: { exception.parameter => exception.message } },
+           status: 422
+  end
+
+  def list_params
+    params.permit(:sort_direction)
   end
 end
