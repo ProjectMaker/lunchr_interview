ce2537d3a0feaed30831d8334b62798e0be437e7
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-06-24 17:34:59 +0200

    Pagination

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,23 +5,27 @@ class TransactionsController < ApplicationController
 
   def index
     validate_list_params
-    render json: Transaction.all.order(created_at: list_params[:sort_direction].to_s),
+    render json: Transaction
+                   .order(created_at: list_params[:sort_direction].to_s)
+                   .page(params.fetch(:page, 1)),
            status: :ok
   end
 
   private
   def validate_list_params
     sort_direction = list_params[:sort_direction]
+    page = list_params[:page].to_i unless list_params[:page].match(/[^[:digit:]]+/)
     has_correct_direction = ['asc', 'desc'].include? sort_direction.downcase
     raise Errors::UnpermittedParameterValue.new(parameter: :sort_direction, value: sort_direction) unless has_correct_direction
+    raise Errors::UnpermittedParameterValue.new(parameter: :page, value: list_params[:page]) unless page.is_a? Integer
   end
 
   def invalid_parameters(exception)
     render json: { errors: { exception.parameter => exception.message } },
-           status: 422
+           status: 400
   end
 
   def list_params
-    params.permit(:sort_direction)
+    params.permit(:sort_direction, :page)
   end
 end
